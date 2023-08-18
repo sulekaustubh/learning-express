2ac688979432d866f536ba649612155e1939289e
sulekaustubh <sulekaustubh97@gmail.com>
2023-08-18 20:02:29 +0530

    json response from external file

---
+++ data.js
@@ -0,0 +1,17 @@
+const names = [
+	{
+		fname: 'Kaustubh',
+	},
+	{
+		fname: 'Sonal',
+	},
+	{
+		fname: 'Arun',
+	},
+	{
+		fname: 'Chitra',
+	},
+];
+
+// syntax to export defaults
+module.exports = { names };
--- index.js
+++ index.js
@@ -1,38 +1,16 @@
 // require() is an in-built function to import modules
 const express = require('express');
-const path = require('path');
+const names = require('./data');
 
 // create an instance of express
 const app = express();
-
-// for using middlewares
-// app.use()
 
 // these routing functions take 2 arguements
 // 1. path
 // 2. callback function that takes request & response
 app.get('/', (req, res) => {
-	// sends the HTTP response
-	res.send('Server is running');
+	res.json(names);
 });
-
-app.get('/contact', (req, res) => {
-	res.send('YOU ARE ON CONTACT ROUTE');
-});
-
-app.get('/file', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/json', (req, res) => {
-	res.json({
-		name: 'Kaustubh',
-	});
-});
-
-app.all('*', (req,res) => {
-    res.send('THIS PAGE DOES NOT EXIST')
-})
 
 // create a server using express
 // listen() is an in-built function of express that takes PORT & a callback function
